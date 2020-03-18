c3e64fa418620a93f78afbd8f5a94a1d548d95de
rosarioof <37800587+rosarioof@users.noreply.github.com>
2020-03-18 12:20:50 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,25 @@
-var a = 40;
+var a =50;
 var c;
-
 function setup() {
-  createCanvas(windowWidht, windowHeight);
+createCanvas(windowWidth, windowHeight);
 }
+// ESTO VENDRIA siendo el espacio de dibujo de 400x400
 
 function draw() {
-  //colorMode (RGB), 
-  colorMode (HSB,360,100,100);
-  //background(0,100,100);
-  fill(random(0,40),100,100);
-  noStroke();
-  c =a * random(1,5);
-  ellipse(mouseX,mouseY,c,c);
  
+  //background(5,240,20);
+ // background(220);esto seria negro
+  //background(0,255,0) verde
+  colorMode(HSB,360,100,100);
+  //background(0,100,100);
+  //fill(0,50,100) pelotitas rojas en el pulsor 
+  fill(random(0,40),100,100);
+  // relleno rojo, con la mitad de saturacion 50, brillo al maximo
+  noStroke();
+  c = a * random(1,5);
+  ellipse(mouseX,mouseY,c,c);
+  
+  
 }
 
 
